9b755856fd0f7c3c46062a2292da98e76f17107e
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-27 19:14:24 -0400

    Se crea Query en interface TareasDAO.

--- app/src/main/java/com/example/roomdatabase/TareasDAO.kt
+++ app/src/main/java/com/example/roomdatabase/TareasDAO.kt
@@ -2,9 +2,13 @@ package com.example.roomdatabase
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface TareasDAO {
     @Insert
     suspend fun  insertarTareas(tareas: Tareas)
+
+    @Query("select * from tabla_tareas order by id ASC")
+    fun getTareas():List<Tareas>
 }
