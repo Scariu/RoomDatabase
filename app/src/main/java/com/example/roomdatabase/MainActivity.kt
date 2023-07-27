d7b96c7737b9d8828658cecfc826373a5133dbad
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-26 20:08:18 -0400

    Se crea nav_graph.xml

--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -2,7 +2,18 @@ package com.example.roomdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+/*
+[ ] Navigation
+[ ] Fragmento agregar
+[ ] Fragmento listado
+[ ] Crear Interfaz
+[ ] Room
+[ ] _Dao
+[ ] _Base Datos
+[ ]_Entity
+[ ] Fragmento listado
+[ ] RecyclerView
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
