aa318b58b1356616cfcf014745872ddeaa71e0ff
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-26 21:51:15 -0400

    Se implementa binding en FirstFragment.

--- app/src/main/java/com/example/roomdatabase/FirstFragment.kt
+++ app/src/main/java/com/example/roomdatabase/FirstFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.roomdatabase.databinding.FragmentFirstBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +18,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FirstFragment : Fragment() {
+    lateinit var binding : FragmentFirstBinding
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -33,9 +35,9 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
-    }
+        binding = FragmentFirstBinding.inflate(layoutInflater)
+        return binding.root
+       }
 
     companion object {
         /**
