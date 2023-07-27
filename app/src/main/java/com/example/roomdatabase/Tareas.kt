954c6b4de19a1832c0eafd819af79b8e19ed4022
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-26 20:47:56 -0400

    Se crea Entity en class Tareas.

--- app/src/main/java/com/example/roomdatabase/Tareas.kt
+++ app/src/main/java/com/example/roomdatabase/Tareas.kt
@@ -1,4 +1,8 @@
 package com.example.roomdatabase
 
-class Tareas {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tabla tareas")
+class Tareas(val nombre: String, val fecha: String)
+@PrimaryKey(autoGenerate = true) var id = 0
