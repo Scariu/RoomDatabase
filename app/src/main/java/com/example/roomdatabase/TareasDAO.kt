d33bbfdd20a28b218fc95df70fc467deb34bf0b9
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-26 20:52:02 -0400

    Se crea método en interface TareasDAO.

--- app/src/main/java/com/example/roomdatabase/TareasDAO.kt
+++ app/src/main/java/com/example/roomdatabase/TareasDAO.kt
@@ -1,4 +1,10 @@
 package com.example.roomdatabase
 
+import androidx.room.Dao
+import androidx.room.Insert
+
+@Dao
 interface TareasDAO {
+    @Insert
+    suspend fun  insertarTareas(tareas: Tareas)
 }
