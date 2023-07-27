41d864dc6a26006d9e32a90afa000f871257abc7
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-26 21:49:06 -0400

    Se implementa class TareasDataBase y se corrigen errores.

--- app/src/main/java/com/example/roomdatabase/Tareas.kt
+++ app/src/main/java/com/example/roomdatabase/Tareas.kt
@@ -3,6 +3,7 @@ package com.example.roomdatabase
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "tabla tareas")
-class Tareas(val nombre: String, val fecha: String)
-@PrimaryKey(autoGenerate = true) var id = 0
+@Entity(tableName = "tabla_tareas")
+data class Tareas(val nombre: String, val fecha: String){
+@PrimaryKey(autoGenerate = true) var id: Long = 1
+}
--- app/src/main/java/com/example/roomdatabase/TareasDataBase.kt
+++ app/src/main/java/com/example/roomdatabase/TareasDataBase.kt
@@ -1,4 +1,32 @@
 package com.example.roomdatabase
 
-class TareasDataBase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [Tareas::class], version = 1)
+abstract class TareasDataBase : RoomDatabase() {
+    abstract fun getTareasDao(): TareasDAO
+    companion object {
+        @Volatile
+        private var INSTANCE: TareasDataBase? = null
+        fun getDataBase(context: Context): TareasDataBase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TareasDataBase::class.java,
+                    "task_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
+
