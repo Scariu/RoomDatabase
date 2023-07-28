562fcb8d382822b50167dc95b1e8aa79b8c687ca
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-27 22:25:14 -0400

    Se implementa TareaViewModel.

--- app/src/main/java/com/example/roomdatabase/TareaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/TareaViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.roomdatabase
 
-class TareaViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class TareaViewModel (aplicacion: Application): AndroidViewModel(aplicacion){
+    private val repositorio: Repositorio
+    init{
+        repositorio = Repositorio(TareasDataBase.getDataBase(aplicacion).getTareasDao())
+    }
+    fun obtenerTareas(): LiveData<List<Tareas>>{
+        return repositorio.listarTareas()
+    }
+    fun insertarTarea(tareas: Tareas)= viewModelScope.launch{
+        repositorio.insertTask(tareas)
+    }
 }
